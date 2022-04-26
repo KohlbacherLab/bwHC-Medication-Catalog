e2229ad3c8ffefd2e7a315733863125e19315980
Lucien <lucien.clin@uni-tuebingen.de>
2022-04-26 16:21:12 +0200

    Corrected potential source of Exceptions in MedicationCatalogImpl: version-based access now uses safe operation

--- impl/src/main/scala/de/bwhc/catalogs/med/impl/MedicationCatalogImpl.scala
+++ impl/src/main/scala/de/bwhc/catalogs/med/impl/MedicationCatalogImpl.scala
@@ -108,73 +108,28 @@ object MedicationCatalogImpl extends MedicationCatalog
     }
 
 
-/*
-  private val atcCodePattern = "[A-Z]{1}[0-9]{2}[A-Z]{2}[0-9]{2}".r
-
-  private val quotedStringPattern = "\".+\\R*.*[^\t]\"".r
-
-
-  private val meds: Map[Year,Iterable[Medication]] =
-    this.synchronized {
-    availableVersions.map { year => 
-
-      val entries =
-        Source.fromInputStream(
-          this.getClass.getClassLoader
-            .getResourceAsStream(s"ATC_GKV_${year}.csv")
-        )
-        .mkString
-        .pipe ( s => 
-
-          quotedStringPattern
-            .replaceAllIn(
-              s,
-              m => {
-                m.matched
-                 .replace("\n","")
-                 .replace("\"","")
-                 .replaceAll("\\s{2,}"," ")
-              }
-            )
-        )
-        .pipe ( s => 
-          Source.fromString(s)
-            .getLines
-            .filter(atcCodePattern.findFirstIn(_).isDefined)
-            .map(_ split separator)
-            .map(
-              csv =>
-                Medication(
-                  Medication.Code(csv(0)),
-                  csv(1),
-                  year
-                )
-            ) 
-            .toList
-        )
-
-        year -> entries
-    }
-    .toMap
-    }
-*/
-
   override def entries(
     version: Year
-  ): Iterable[Medication] = meds(version)
+  ): Iterable[Medication] =
+    meds.get(version)
+      .getOrElse(Seq.empty)
 
 
   override def find(
     code: Medication.Code,
     version: Year
   ): Option[Medication] =
-    meds(version).find(_.code == code)
+    meds.get(version)
+      .flatMap(_.find(_.code == code))
 
 
   override def findMatching(
     pattern: String,
     version: Year
   ): Iterable[Medication] =
-    meds(version).filter(_.name.contains(pattern))
+    meds.get(version)
+      .map(_.filter(_.name.contains(pattern)))
+      .getOrElse(Seq.empty)
+
 
 }
